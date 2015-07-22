48538d84463467656f024b396112b99dc8346551
Ioana Crant <ioana.crant@gmail.com>
2015-07-22 15:27:45 -0700

    first

---
+++ Countinversions.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class Countinversions{
+  public static long mergeAndSort(long[]a, long[]b, long[]result){
+    int i=0;
+    int j=0;
+    long count=0;
+    for (int k=0;k<result.length;k++){
+      if (j==b.length){
+        result[k]=a[i];
+        i++;
+      }
+      else if(i==a.length){
+        result[k]=b[j];
+        j++;
+      }
+      else if (a[i]<b[j]){
+        result[k]=a[i];
+        i++;
+      }
+      else{
+        result[k]=b[j];
+        count += a.length-i;
+        j++;
+      }
+    }
+    return count;
+  }
+  public static long countInversions(long[] orig){
+    if (orig.length<=1){
+      return 0;
+    }
+    long[] a=new long[orig.length/2];
+    System.arraycopy(orig,0,a,0,a.length);
+    long[] b=new long[orig.length-orig.length/2];
+    System.arraycopy(orig,a.length,b,0,b.length);
+    long countLeft=countInversions(a);
+    long countRight=countInversions(b);
+
+    long[] result=new long[orig.length];
+    long countMerge=mergeAndSort(a,b,result);
+
+    for (int i=0;i<result.length;i++){
+      orig[i]=result[i];
+    }
+
+    return countLeft+countRight+countMerge;
+  }
+  public static void main(String[] args){
+    BufferedReader br=null;
+    try{
+      br = new BufferedReader(new FileReader("IntegerArray.txt"));
+      long[] orig=new long[100000];
+      for (int i=0;i<100000;i++){
+        orig[i]=Long.parseLong(br.readLine());
+      }
+      System.out.println(countInversions(orig));
+    }
+    catch (IOException e){
+      e.printStackTrace();
+    }
+
+  }
+}
---
+++ Mergesort.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+public class Mergesort{
+  public static int[] merge(int[]a, int[]b){
+    int i=0;
+    int j=0;
+    int[] mergedArray=new int[a.length+b.length];
+    for (int k=0;k<mergedArray.length;k++){
+      if (j==b.length){
+        mergedArray[k]=a[i];
+        i++;
+      }
+      else if(i==a.length){
+        mergedArray[k]=b[j];
+        j++;
+      }
+      else if (a[i]<b[j]){
+        mergedArray[k]=a[i];
+        i++;
+      }
+      else{
+        mergedArray[k]=b[j];
+        j++;
+      }
+    }
+    return mergedArray;
+  }
+  public static int[] split(int[] orig){
+    if (orig.length==0 || orig.length==1){
+      return orig;
+    }
+    int[] a=new int[orig.length/2];
+    System.arraycopy(orig,0,a,0,a.length);
+    int[] b=new int[orig.length-orig.length/2];
+    System.arraycopy(orig,a.length,b,0,b.length);
+    return merge(split(a),split(b));
+  }
+  public static void main(String[] args){
+    int[] orig= {2,5,3,3,8,1,7,4,9,6};
+    int[] a= {1,3};
+    int[] b= {2,4};
+    int[] x=merge(a,b);
+    System.out.println(Arrays.toString(split(orig)));
+
+  }
+}
