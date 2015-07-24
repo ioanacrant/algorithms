fd975a4cdcdc431d6f2d251fd9f82e8cafb61638
Ioana Crant <ioana.crant@gmail.com>
2015-07-24 13:57:56 -0700

    q2

--- QuickSort.java
+++ QuickSort.java
@@ -1,14 +1,15 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 public class QuickSort{
+    static int count;
     public static void partition(int[] array, int start, int end){
         if(end-start>=2){
-            Random rand= new Random();
-            int pivotIndex = rand.nextInt(end-start)+start;
-
-            array=swap(array,start,pivotIndex);
+            int pivotIndex = end-1;
+            swap(array, start, pivotIndex);
             int pivot = array[start];
-            System.out.println(pivot);
             int i=start+1;
             for (int j=start+1;j<end;j++){
                 if (array[j]<pivot){
@@ -17,10 +18,12 @@ public class QuickSort{
                 }
             }
             swap(array,start,i-1);
-
-            System.out.println(Arrays.toString(array));
+            count+=i-1-start;
+            count+=end-i;
             partition(array,start,i-1);
+
             partition(array,i,end);
+
         }
     }
     public static int[] swap(int[] array, int index1, int index2){
@@ -31,8 +34,26 @@ public class QuickSort{
         return array;
     }
     public static void main(String[] args){
-        int[]a1 = {6,2,3,7,1,5,9};
+        int[]a1 = {6,2,3,1};
         partition(a1,0,a1.length);
         System.out.println(Arrays.toString(a1));
+        System.out.println(count);
+
+        BufferedReader br=null;
+        try{
+          br = new BufferedReader(new FileReader("QuickSort.txt"));
+          int[] orig=new int[10000];
+          for (int i=0;i<10000;i++){
+            orig[i]=Integer.parseInt(br.readLine());
+          }
+          partition(orig,0,orig.length);
+          System.out.println(count);
+        }
+        catch (IOException e){
+          e.printStackTrace();
+        }
+
+
+
     }
 }
