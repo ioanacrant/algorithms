3e73351cae98bc6a8b969290e3a4c03d93a497b8
Ioana Crant <ioana.crant@gmail.com>
2015-08-14 22:06:59 -0700

    dijkstras

--- coursera/Dijkstras.java
+++ coursera/Dijkstras.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Dijkstras{
 
     public static void main(String[] args) throws IOException{
-        Scanner inf = new Scanner(new BufferedReader(new FileReader("coursera/dijkstrastest.txt")));
+        Scanner inf = new Scanner(new BufferedReader(new FileReader("coursera/dijkstraData.txt")));
 
         ArrayList<ArrayList<ArrayList<Integer>>> adjacencyList = new ArrayList<ArrayList<ArrayList<Integer>>>();
         adjacencyList.add(new ArrayList<ArrayList<Integer>>());
@@ -12,7 +12,7 @@ public class Dijkstras{
         while (inf.hasNextLine()){
             count+=1;
             ArrayList<ArrayList<Integer>> weightedEdges = new ArrayList<ArrayList<Integer>>();
-            String[] line = inf.nextLine().split(" ");
+            String[] line = inf.nextLine().split("\t");
             for (int i =1; i<line.length;i++){
                 ArrayList<Integer> edge = new ArrayList<Integer>();
                 String[] pair= line[i].split(",");
@@ -23,13 +23,43 @@ public class Dijkstras{
             }
             adjacencyList.add(weightedEdges);
         }
-
+        System.out.println(adjacencyList);
         ArrayList<Integer> processedVertices = new ArrayList<Integer>();
         int[] shortestPathDistances = new int[count+1];
         Arrays.fill(shortestPathDistances,1000000);
         shortestPathDistances[1]=0;
 
-        System.out.println(Arrays.toString(shortestPathDistances));
+        processedVertices.add(1);
+        while (processedVertices.size()<count){
+            int lowestGreedyScore = 1000000000;
+            int lowestOutsideEdge=-1;
+            // System.out.println(processedVertices);
+            for (int n:processedVertices){
+                // System.out.println(n);
+                ArrayList<ArrayList<Integer>> edgesFromN = adjacencyList.get(n);
+                // System.out.println(edgesFromN);
+                for (ArrayList<Integer> edgePair: edgesFromN){
+                    // System.out.print("h");
+                    // System.out.println(edgePair);
+                    if (!processedVertices.contains(edgePair.get(0) )){
+                        int computedScore = shortestPathDistances[n] + edgePair.get(1);
+                        if (computedScore<lowestGreedyScore){
+                            lowestGreedyScore = computedScore;
+                            lowestOutsideEdge = edgePair.get(0);
+                        }
+                        // System.out.println(lowestGreedyScore);
+                    }
+                }
+            }
 
+            processedVertices.add(lowestOutsideEdge);
+            shortestPathDistances[lowestOutsideEdge] = lowestGreedyScore;
+
+        }
+        System.out.print(Arrays.toString(shortestPathDistances));
+        int[] cool = {7,37,59,82,99,115,133,165,188,197};
+        for (int c:cool){
+            System.out.printf("%d,",shortestPathDistances[c]);
+        }
     }
 }
