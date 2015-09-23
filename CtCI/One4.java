74656be1ac1e0fc600540971002decab006c7ea7
Ioana Crant <ioana.crant@gmail.com>
2015-09-22 22:45:24 -0400

    leet and ctci1-4

--- CtCI/One4.java
+++ CtCI/One4.java
@@ -1,18 +1,29 @@
 import java.util.*;
 
 public class One4{
-    public static String replace(char[] string, int length){
-        char[] charA = a.toCharArray();
-        System.out.print(charA[1]);
-        for (int i=0;i<charA.length();i++){
-            if (charA[i]==' '){
-                charA[i]='%20';
+    public static char[] replace(char[] string){
+        for (int i=0;i<string.length;i++){
+            if (string[i]==' '){
+                shift(i, string);
+                System.out.println(string);
+                string[i]='%';
+                string[i+1]='2';
+                string[i+2]='0';
             }
         }
-        System.out.print(charA.toString());
-        return a;
+        return string;
+    }
+
+    public static void shift(int n, char[] s){
+        for(int i=s.length-1;i>n;i--){
+            if (s[i]!=' '){
+                s[i+2]=s[i];
+            }
+        }
     }
     public static void main(String[] args){
-        replace("tgds");
+        char[] a = "a bc d    ".toCharArray();
+        System.out.print(replace(a));
+
     }
 }
---
+++ leetcode/MoveZeroes.java
@@ -0,0 +1,8 @@
+public class MoveZeroes{
+    public static void moveZeroes(int[] nums){
+        
+    }
+    public static void main(String[] args){
+
+    }
+}
