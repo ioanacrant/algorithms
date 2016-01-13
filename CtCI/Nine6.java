ff4dfac228a3055b8382e1464c61766c76698e0f
Ioana Crant <ioana.crant@gmail.com>
2016-01-12 23:48:25 -0500

    nine 6

---
+++ CtCI/Nine6.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Nine6{
+    public static void main(String[] args){
+        System.out.print(combParentheses(3));
+    }
+    public static ArrayList<String> combParentheses(int n){
+        if (n==1){
+            ArrayList<String> a = new ArrayList<String>();
+            a.add("()");
+            return a;
+        }
+        return addParen(combParentheses(n-1));
+    }
+    public static ArrayList<String> addParen(ArrayList<String> before){
+        ArrayList<String> after = new ArrayList<String>();
+        for (String p:before){
+            after.add("("+p+")");
+            after.add(p+"()");
+            if (isNotAPalindrome(p+"()")){
+                after.add("()"+p);
+            }
+        }
+        return after;
+    }
+    public static boolean isNotAPalindrome(String p){
+        for (int i=0;i<p.length();i++){
+            if (p.charAt(i)==p.charAt(p.length()-i-1)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
